374e94f0ccfa74e18e98e933a28a48e3b4a10675
diegoefraingonzal <13064@portalcedi.edu.mx>
2025-03-11 10:07:37 -0600

    17commit

--- S16/src/app/home/home.page.ts
+++ S16/src/app/home/home.page.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  taskForm: FormGroup;
+  tasks: Array<{ title: string; dueDate: string; done: boolean }> = [];
+  filter: string = 'all';
+
+  constructor(private fb: FormBuilder) {
+    this.taskForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      dueDate: ['', Validators.required]
+    });
+  }
+
+  addTask() {
+    if (this.taskForm.valid) {
+      this.tasks.push({
+        title: this.taskForm.value.title,
+        dueDate: this.taskForm.value.dueDate,
+        done: false
+      });
+      this.taskForm.reset();
+    }
+  }
+
+  toggleCompletion(index: number) {
+    this.tasks[index].done = !this.tasks[index].done;
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+
+  getFilteredTasks() {
+    if (this.filter === 'completed') {
+      return this.tasks.filter(task => task.done);
+    } else if (this.filter === 'pending') {
+      return this.tasks.filter(task => !task.done);
+    }
+    return this.tasks;
+  }
 }
--- S16/src/environments/environment.ts
+++ S16/src/environments/environment.ts
@@ -1,10 +1,28 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+// Import the functions you need from the SDKs you need
 
-export const environment = {
-  production: false
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBHyYXBkELYvHMxSsoZill4ZR9mu-49h64",
+  authDomain: "ea16-9f3f1.firebaseapp.com",
+  projectId: "ea16-9f3f1",
+  storageBucket: "ea16-9f3f1.firebasestorage.app",
+  messagingSenderId: "321525471172",
+  appId: "1:321525471172:web:491f28463330a295707691",
+  measurementId: "G-5Q4JQGCBTT"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 /*
  * For easier debugging in development mode, you can import the following file
